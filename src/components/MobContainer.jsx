0d8bcadc1794bf9d24fe2e807bf7fb553c8f8c5d
atroopa <atroopa@gmail.com>
2023-04-27 17:14:37 +0330

    install tailwind css

---
+++ src/components/MobContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const mobContainer = () => {
+  return (
+    <div className='justify-center items-center'>
+      <h1>number of Mobile</h1>
+      <button>Buy</button>
+    </div>
+  );
+}
+
+export default mobContainer;
