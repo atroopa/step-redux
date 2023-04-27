08e83efcaafdb112f0a530c7da60e6ca51ede1dc
atroopa <atroopa@gmail.com>
2023-04-27 17:40:07 +0330

    component now is center

--- src/components/MobContainer.jsx
+++ src/components/MobContainer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const mobContainer = () => {
+const MobContainer = () => {
   return (
-    <div className='justify-center items-center'>
-      <h1>number of Mobile</h1>
-      <button>Buy</button>
+    <div className='mt-60 flex flex-col justify-center items-center '>
+      <h1 className="py-10 text-4xl font-bold">
+        number of Mobile
+      </h1>
+      
+      <button className='bg-gray-700 hover:bg-gray-900 text-white py-2 px-4 rounded-2xl'>
+        Buy
+      </button>
+
     </div>
   );
 }
 
-export default mobContainer;
+export default MobContainer;
